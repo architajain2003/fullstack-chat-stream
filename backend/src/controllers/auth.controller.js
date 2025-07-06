3989432c62feffebffe77877b1fb15f06c22b42e
architajain2003 <59245825+architajain2003@users.noreply.github.com>
2025-07-06 16:39:52 +0530

    auth changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -53,8 +53,10 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      // sameSite: "strict", prevent CSRF attacks
+      sameSite: "None",
+      secure : true,
+      // secure: process.env.NODE_ENV === "production",
     });
 
     res.status(201).json({ success: true, user: newUser });
